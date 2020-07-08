de9a9c9204c564d1e54c6330bc94909ae0a270ec
Guilherme Souza <67706571+gssAutomation@users.noreply.github.com>
2020-07-08 18:52:39 -0300

    Aullas

---
+++ AJ022_ObjetosClass.java
@@ -0,0 +1,42 @@
+/**
+ * AJ022_ObjetosClass
+ */
+public class AJ022_ObjetosClass {
+
+    public static void main(String[] args) {
+        //Delacração e /instanciando/iniciação do objeto;
+        //p1 -> instancia do objeto
+        Produto p0;
+        Produto p1 = new Produto();
+
+        p1.nome = "Notebook";
+        p1.preco = 2500f;
+        p1.desconto = 15.0f;
+
+        System.out.println("--------Produto--------");
+        System.out.println(p1.nome);
+        System.out.println("R$ " + p1.preco);
+        System.out.println(p1.desconto + " %");
+
+        p0 = new Produto();
+        p0.nome = "Caneta Bic";
+        p0.preco = 10.0f;
+        p0.desconto = 10.0f;
+
+        System.out.println("--------Produto--------");
+        System.out.println(p0.nome);
+        System.out.println("R$ " + p0.preco);
+        System.out.println(p0.desconto + " %");
+
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Angelina Jolie";
+        pessoa1.email = "angelina.com";
+        pessoa1.ano_nascimento = 1990;
+
+        System.out.println();
+        System.out.println("--------Pessoa--------");
+        System.out.println("Nome: " + pessoa1.nome);
+        System.out.println("E-mail: " + pessoa1.email);
+        System.out.println("Ano de Nascimento " + pessoa1.ano_nascimento);
+    } 
+}
---
+++ Pessoa.java
@@ -0,0 +1,24 @@
+/*
+//Aula de classes
+
+//Classes
+- o nome das classes inicia com letra Maiuscula;
+- o nome não deve conter: Acentuação, caracteres especiais, espaço;
+- Nas classes java, não existe a implementação da função main;
+- Toda classe Java possui a seguinte forma;
+- o nome da classe deve ser o mesmo nome do arquivo java;
+- public class NomeDaClasse{
+    exemplo
+}
+//Atributos
+- São as características da classe/molde/modelo de dados;
+- Podemos entender atributos como variáveis da classe;
+- Uma outra forma de nomenclatura para os atributos são estados;
+- Atributos são nomeados em letras minusculas, sem espaço, sem caracteres especiais, sem acentuação;
+*/
+
+public class Pessoa {
+    String nome, email;
+    int ano_nascimento;
+    
+}
